435a4030c6b8db1b2ccc8359b889be36982adf54
ChaitraliTikar <chaitrali678@gmail.com>
2026-03-29 14:11:35 +0530

    fix: added jwt import in auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,11 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const Company = require("../models/Company");
 const User = require("../models/user");
+
 const signupSchema = require("../validators/signupSchema");
+const loginSchema = require("../validators/loginSchema");
 
 exports.signup = async (req, res) => {
   try {
@@ -33,6 +37,61 @@ exports.signup = async (req, res) => {
 
     res.status(201).json({
       message: "Signup successful",
+      user
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const parsed = loginSchema.safeParse(req.body);
+
+    if (!parsed.success) {
+      return res.status(400).json({
+        error: parsed.error.errors
+      });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({
+      where: { email }
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        error: "User not found"
+      });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      return res.status(401).json({
+        error: "Invalid credentials"
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        role: user.role,
+        company_id: user.company_id
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d"
+      }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
       user
     });
 
